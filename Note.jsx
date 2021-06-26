d235f57cadac0cd050ffee804ce6262af79e1540
Yukesh Devendran <79779245+Yukesh-D@users.noreply.github.com>
2021-06-26 21:04:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>MERN</h1>
+      <p>
+        MERN stack is a kind of Web Development: M-Mongodb E-Express R-React.js
+        N-Node.js
+      </p>
+    </div>
+  );
+}
+
+export default Note;
